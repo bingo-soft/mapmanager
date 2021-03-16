042148868fb99bcb4e6c75bc64083aac3ff49ad9
root <a.kozlov@bingosoft.ru>
2021-03-16 18:23:08 +0300

    feat: layer styles support

---
+++ src/Domain/Model/Style/StyleType.ts
@@ -0,0 +1,11 @@
+import { Style } from "ol/style";
+export type StyleType = {
+    "Point": Style,
+    "MultiPoint": Style,
+    "LineString": Style,
+    "MultiLineString": Style,
+    "Polygon": Style,
+    "MultiPolygon": Style,
+    "Circle": Style,
+    "GeometryCollection": Style
+}
