25e2b1d8b4dad3ad3f3a7be26b6a53cf4fb08993
Aleksandr Kozlov <a.kozlov@bingosoft.ru>
2021-10-01 20:20:23 +0300

    fix: bbox strategy = all

--- src/Domain/Model/Source/Impl/VectorSource.ts
+++ src/Domain/Model/Source/Impl/VectorSource.ts
@@ -12,7 +12,8 @@ export default class VectorSource extends BaseSource {
         super();
         this.source = new OlVectorSource({
             format: new OlGeoJSON(),
-            strategy: OlLoadingstrategy.bbox
+            //strategy: OlLoadingstrategy.bbox
+            strategy: OlLoadingstrategy.all
         });
     }
 
