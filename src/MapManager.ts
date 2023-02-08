ba61676aad9bb88eb4335520001f478565b32b4b
koalex76 <ko-alex@mail.ru>
2023-02-08 16:21:37 +0300

    chore: export() method returns base64 file representation

--- src/MapManager.ts
+++ src/MapManager.ts
@@ -143,9 +143,11 @@ export default class MapManager {
      * @category Map
      * @param map - map instance
      * @param exportType - type of export, defaults to ExportType.Printer
+     * @param isDownload - parameter indicating whether the file should be downloaded by a browser
+     * @return in case of PNG or GeoTIFF a promise with the file information 
      */
-    public static async export(map: Map, exportType: ExportType = ExportType.Printer): Promise<unknown> {
-        return await map.export(exportType);
+    public static async export(map: Map, exportType: ExportType = ExportType.Printer, isDownload: boolean = true): Promise<unknown> {
+        return await map.export(exportType, isDownload);
     }
 
     /**
